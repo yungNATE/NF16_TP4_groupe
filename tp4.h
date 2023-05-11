cc27ffa7d3bb6e1085cc4e4732fc963b9479eb84
yungNATE <nathan.martinigol@gmail.com>
2023-05-11 08:56:02 +0200

    correction emplacement structures

---
+++ tp4.h
@@ -0,0 +1,25 @@
+// Structures et types
+struct Position {
+    int numeroLigne;
+    int ordre;
+    int numeroPhrase;
+    struct Position* suivant;
+};
+typedef struct Position T_Position;
+
+struct Noeud {
+    char* mot;
+    int nbOccurences;
+    T_Position* listePositions;
+    struct Noeud* filsGauche;
+    struct Noeud* filsDroit;
+};
+typedef struct Noeud T_Noeud;
+
+
+struct Index {
+    T_Noeud* racine;
+    int nbMotsDistincts;
+    int nbMotsTotal;
+};
+typedef struct Index T_Index;
