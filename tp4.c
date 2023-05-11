1d53ad3bea8a9618365c7718deb8d1a45fe2b142
yungNATE <nathan.martinigol@gmail.com>
2023-05-11 09:19:29 +0200

    création fonction

--- tp4.c
+++ tp4.c
@@ -4,3 +4,14 @@
 #include <stdbool.h>
 #include "tp4.h"
 
+// Fonction permettant l'ajout d'un mot dans l'index en respectant les règles d'insertion dans les ABR. 
+// Elle renvoie 1 en cas de succès, 0 sinon.
+int ajouterOccurence(T_Index *index, char *mot, int ligne, int ordre, int phrase){
+    bool ajoutFlag = false;
+    bool nouveauMot = false;
+
+    if(ajoutFlag){
+        index->nbMotsTotal++;
+        if(nouveauMot) index->nbMotsDistincts++;
+    }
+}
