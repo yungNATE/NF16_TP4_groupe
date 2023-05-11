3594bd38cd20fd28b9304dd1ee8ba662d02076da
yungNATE <nathan.martinigol@gmail.com>
2023-05-11 09:59:29 +0200

    WIP

--- tp4.c
+++ tp4.c
@@ -10,9 +10,44 @@ int ajouterOccurence(T_Index *index, char *mot, int ligne, int ordre, int phrase
     bool ajoutFlag = false;
     bool nouveauMot = false;
 
+    T_Noeud *noeud = index->racine;
+    parcourirArbre(noeud, mot, &ajoutFlag, &nouveauMot, ligne, ordre, phrase);
+
     if(ajoutFlag){
         index->nbMotsTotal++;
         if(nouveauMot) index->nbMotsDistincts++;
     }
 }
-// toto
+
+// Fonction  permettant de parcourir l'arbre et d'ajouter un mot si besoin.
+parcourirArbre(T_Noeud *noeud, char *mot, bool *ajoutFlag, bool *nouveauMot, int ligne, int ordre, int phrase){
+    if(noeud == NULL){
+        noeud = creerNoeud(mot);
+        noeud->nbOccurences++;
+        noeud->nbMotsTotal++;
+        noeud->nbMotsDistincts++;
+        noeud->ligne = ligne;
+        noeud->ordre = ordre;
+        noeud->phrase = phrase;
+        *ajoutFlag = true;
+        *nouveauMot = true;
+    }
+    else if(strcmp(mot, noeud->mot) < 0){
+        noeud->nbMotsTotal++;
+        noeud->nbMotsDistincts++;
+        parcourirArbre(noeud->gauche, mot, ajoutFlag, nouveauMot, ligne, ordre, phrase);
+    }
+    else if(strcmp(mot, noeud->mot) > 0){
+        noeud->nbMotsTotal++;
+        noeud->nbMotsDistincts++;
+        parcourirArbre(noeud->droite, mot, ajoutFlag, nouveauMot, ligne, ordre, phrase);
+    }
+    else if(strcmp(mot, noeud->mot) == 0){
+        noeud->nbOccurences++;
+        noeud->ligne = ligne;
+        noeud->ordre = ordre;
+        noeud->phrase = phrase;
+        *ajoutFlag = true;
+        *nouveauMot = false;
+    }
+}
