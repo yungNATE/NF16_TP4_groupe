5a1ed7ffd0ccb275371bf22928d43f17524a81f9
alex16-1 <a.ghitu@yahoo.com>
2023-05-15 12:24:45 +0200

    ajouterPosition

--- main.c
+++ main.c
@@ -4,3 +4,19 @@
 #include <stdbool.h>
 #include "tp4.h"
 #include "tp4.c"
+
+int main(void){
+
+    T_Position *test = creerPosition(2,1,1);
+
+    test = ajouterPosition(test, 1,4,1);
+    test = ajouterPosition(test, 2,4,2);
+    test = ajouterPosition(test, 10,4,5);
+    test = ajouterPosition(test, 3,4,2);
+    // Tests pour l'erreur d'ordre de phrase:
+    // test = ajouterPosition(test, 1,2,2);
+    // test = ajouterPosition(test, 1,3,2);
+
+    afficherPosition(test);
+    return 0;
+}
--- tp4.c
+++ tp4.c
@@ -4,47 +4,107 @@
 #include <stdbool.h>
 #include "tp4.h"
 
+void afficherPosition(T_Position *listeP){
+    while (listeP != NULL){
+        printf("Ligne: %d, Ordre: %d, Phrase: %d\n", listeP->numeroLigne, listeP->ordre, listeP->numeroPhrase);
+        listeP = listeP->suivant;
+    }
+}
+
+T_Position *creerPosition(int ligne, int ordre, int phrase){
+
+    T_Position *nouveau = malloc(sizeof(T_Position));
+    if (nouveau == NULL) return NULL;
+
+    nouveau->numeroLigne = ligne;
+    nouveau->ordre = ordre;
+    nouveau->numeroPhrase = phrase;
+    nouveau->suivant = NULL;
+    
+    return nouveau;
+}
+
 T_Position *ajouterPosition(T_Position *listeP, int ligne, int ordre, int phrase){
 
     // Création position
 
-    T_Position *nouveau = malloc(sizeof(T_Position));
-    if (nouveau == NULL) return NULL;
-    nouveau->numeroLigne = ligne;
-    nouveau->ordre = ordre;
-    nouveau->numeroPhrase = phrase;
-
+    T_Position *nouveau = creerPosition(ligne, ordre, phrase);
     T_Position *iter = listeP;
     T_Position *precedent = NULL;
 
+    // Pour tenir compte du bon ordre des phrases
+
+    int track_phrase = 0;
 
     // Chercher la bonne position
+
     while (iter != NULL){
-        if (iter->numeroLigne < ligne){
+        if (iter->numeroLigne < ligne) // Parcourir les lignes
+        {   
+            if (iter->numeroPhrase > track_phrase) {track_phrase = iter->numeroPhrase;}
             precedent = iter;
             iter = iter->suivant;
         }
 
-        if (iter->numeroLigne = ligne){
-            if (iter->ordre < ordre){
+        else if (iter->numeroLigne == ligne) // On a trouvé la bonne ligne
+        { 
+            if (iter->ordre < ordre) // Parcourir les ordres
+            {   
+                if (iter->numeroPhrase > track_phrase) {track_phrase = iter->numeroPhrase;}
                 precedent = iter;
                 iter = iter->suivant;
             }
 
-            else if (iter->ordre == ordre){
-                // Déjà existe
+            else if (iter->ordre == ordre)
+            {
+                printf("Existe déjà\n");
+                return listeP;
             }
 
-            else {
-                // Insert and return
+            else if (iter->ordre > ordre) 
+            {   
+                // Si jamais on veut introduire dans une même ligne, un mot avant des mots déjà existants
+                // Exemple: 
+                // Sur la première ligne il y a seulement ce noeud:
+                // Ligne: 1, Ordre: 4, Phrase: 10 
+                // Et on souhaite rajouter, pour une raison ou autre, le noeud:
+                // Ligne: 1, Ordre: 3, Phrase: 10
+                break;
             }
+
+        }
+
+        else
+        {   
+            // Ici on arrive si jamais iter->numeroLigne > ligne. Cela arrive quand:
+
+            // Pour une raison quelconque, on souhaite insérer en tête
+            // La ligne que l'on veut insérer n'existe pas et on va l'insérer entre 2 lignes existantes en respectant l'ordre
+
+            break;
         }
     }
 
-    // Si on n'a pas retrouvé la bonne ligne c'est qu'elle existe pas
-    // Ici donc iter = NULL et precedent est le dernier élément
+    // On est à la bonne ligne et au bon ordre
 
+    if (track_phrase > phrase || (iter !=NULL && iter->numeroPhrase < phrase))
+    {   
+        printf("ERREUR: ordre incohérent des phrases.\n");
+        printf("Bon ordre de ligne et de mot mais l'ordre de phrase que vous insérez est plus petit que celui à l'endroit où vous insérez.\n");
+        printf("L'erreur concerne l'insertion ");
+        afficherPosition(nouveau);
+        return listeP;
+    }
+
+    if (precedent == NULL) // On rajoute en tête de liste
+    { 
+        nouveau->suivant = listeP;
+        return nouveau;
+    }
+
+    // Sinon on est au milieu ou à la fin
+
+    nouveau->suivant = iter;
     precedent->suivant = nouveau;
     return listeP;
 }
-// toto
