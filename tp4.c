77225551b941d68f4ba1b01bdc1fbeadc13a538e
yungNATE <nathan.martinigol@gmail.com>
2023-05-24 16:14:02 +0200

    "ajouterOccurence" WIP : à tester

--- tp4.c
+++ tp4.c
@@ -7,47 +7,75 @@
 // Fonction permettant l'ajout d'un mot dans l'index en respectant les règles d'insertion dans les ABR. 
 // Elle renvoie 1 en cas de succès, 0 sinon.
 int ajouterOccurence(T_Index *index, char *mot, int ligne, int ordre, int phrase){
-    bool ajoutFlag = false;
-    bool nouveauMot = false;
-
-    T_Noeud *noeud = index->racine;
-    parcourirArbre(noeud, mot, &ajoutFlag, &nouveauMot, ligne, ordre, phrase);
-
-    if(ajoutFlag){
-        index->nbMotsTotal++;
-        if(nouveauMot) index->nbMotsDistincts++;
+     // Conversion du mot en minuscules pour l'ignorance de la casse
+    int i;
+    int tailleMot = strlen(mot);
+    for (i = 0; i < tailleMot; i++) {
+        mot[i] = tolower(mot[i]);
     }
-}
-
-// Fonction  permettant de parcourir l'arbre et d'ajouter un mot si besoin.
-parcourirArbre(T_Noeud *noeud, char *mot, bool *ajoutFlag, bool *nouveauMot, int ligne, int ordre, int phrase){
-    if(noeud == NULL){
-        noeud = creerNoeud(mot);
-        noeud->nbOccurences++;
-        noeud->nbMotsTotal++;
-        noeud->nbMotsDistincts++;
-        noeud->ligne = ligne;
-        noeud->ordre = ordre;
-        noeud->phrase = phrase;
-        *ajoutFlag = true;
-        *nouveauMot = true;
+    
+    // Création de la nouvelle position
+    T_Position* nouvellePosition = (T_Position*)malloc(sizeof(T_Position));
+    nouvellePosition->numeroLigne = ligne;
+    nouvellePosition->ordre = ordre;
+    nouvellePosition->numeroPhrase = phrase;
+    nouvellePosition->suivant = NULL;
+    
+    // Recherche de l'emplacement d'insertion dans l'ABR
+    T_Noeud *courant = index->racine;
+    T_Noeud *precedent = NULL;
+    
+    while (courant != NULL) {
+        int comparaison = strcmp(mot, courant->mot);
+        
+        if (comparaison == 0) {
+            // Le mot existe déjà dans l'index, ajout de la nouvelle position
+            T_Position *positionCourante = courant->listePositions;
+            T_Position *positionPrecedente = NULL;
+            
+            // Parcours de la liste chaînée des positions pour trouver la position à insérer
+            while (positionCourante != NULL) {
+                positionPrecedente = positionCourante;
+                positionCourante = positionCourante->suivant;
+            }
+            
+            if (positionPrecedente != NULL) {
+                positionPrecedente->suivant = nouvellePosition;
+            } else {
+                courant->listePositions = nouvellePosition;
+            }
+            
+            courant->nbOccurences++;
+            index->nbMotsTotal++;
+            
+            return 1; // Succès de l'ajout
+        } else {
+            precedent = courant;
+            courant = courant->filsDroit;
+        }
     }
-    else if(strcmp(mot, noeud->mot) < 0){
-        noeud->nbMotsTotal++;
-        noeud->nbMotsDistincts++;
-        parcourirArbre(noeud->gauche, mot, ajoutFlag, nouveauMot, ligne, ordre, phrase);
+    
+    // Le mot n'existe pas dans l'index, création d'un nouveau nœud
+    T_Noeud *nouveauNoeud = (T_Noeud*)malloc(sizeof(T_Noeud));
+    nouveauNoeud->mot = strdup(mot);
+    nouveauNoeud->nbOccurences = 1;
+    nouveauNoeud->listePositions = nouvellePosition;
+    nouveauNoeud->filsGauche = NULL;
+    nouveauNoeud->filsDroit = NULL;
+    
+    if (precedent == NULL) {
+        index->racine = nouveauNoeud;
+    } else {
+        int comparaisonPrecedent = strcmp(mot, precedent->mot);
+        if (comparaisonPrecedent < 0) {
+            precedent->filsGauche = nouveauNoeud;
+        } else {
+            precedent->filsDroit = nouveauNoeud;
+        }
     }
-    else if(strcmp(mot, noeud->mot) > 0){
-        noeud->nbMotsTotal++;
-        noeud->nbMotsDistincts++;
-        parcourirArbre(noeud->droite, mot, ajoutFlag, nouveauMot, ligne, ordre, phrase);
-    }
-    else if(strcmp(mot, noeud->mot) == 0){
-        noeud->nbOccurences++;
-        noeud->ligne = ligne;
-        noeud->ordre = ordre;
-        noeud->phrase = phrase;
-        *ajoutFlag = true;
-        *nouveauMot = false;
-    }
+    
+    index->nbMotsDistincts++;
+    index->nbMotsTotal++;
+    
+    return 1; // Succès
 }
